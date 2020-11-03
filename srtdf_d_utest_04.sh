ffffb789aba5d5ad2134ce4ef72ade6770b44fc0
ksridhar <ksridhar@amagi.com>
2020-11-03 15:25:38 +0530

    added srtdf_d_utest_04.sh

---
+++ srtdf_d_utest_04.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -u
+#set -x
+
+#----[globals]---------------------------------------------------------------
+
+DIRNAME=$(dirname $(readlink -e $0))
+BASENAME=$(basename $0)
+BARENAME=${BASENAME%%.*}
+UTESTNUM=${BARENAME##*_}
+
+SAMPLE_SRT_DIFF_FILEPATH=$DIRNAME/sample_srt_diff.txt
+
+#----[temp files and termination]--------------------------------------------
+
+function fnxOnEnd
+{
+    tap_utest_ends
+
+    rm $TMP1 $TMP2
+}
+
+TMP1=`mktemp`
+TMP2=`mktemp`
+
+trap 'fnxOnEnd;' 0 1 2 3 6 9 11
+
+#----[main]-----------------------------------------------------------------
+
+cd $DIRNAME
+export PATH=$PATH:$PWD
+
+source common_bash_functions.sh
+source common_tap_functions.sh
+source srtdf_d_utest_common_functions.sh
+
+tap_utest_begins
+
+cat $SAMPLE_SRT_DIFF_FILEPATH | python3 sstt_x_srt_compare_reader.py >$TMP1 2>&1
+
+if ! is_utest_output_ok $TMP1
+then
+    tap_utest_failed
+    exit 1
+fi
+
+tap_utest_passed
+exit 0
+
