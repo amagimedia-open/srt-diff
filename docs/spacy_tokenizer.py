96f62e2ad2778954e5ea1a02800f134a492d481d
ksridhar <ksridhar@amagi.com>
2021-01-29 16:56:53 +0530

    loading language model in spacy dynamically

--- docs/spacy_tokenizer.py
+++ docs/spacy_tokenizer.py
@@ -1,5 +1,14 @@
-from spacy.lang.en import English
-nlp = English()
+#from spacy.lang.en import English
+#import spacy.lang.en
+#nlp = spacy.lang.en.English()
+
+#at command line
+#python3 -m spacy download en
+
+import spacy
+nlp = spacy.load("en")
+
+
 # Create a Tokenizer with the default settings for English
 # including punctuation rules and exceptions
 tokenizer = nlp.Defaults.create_tokenizer(nlp)
